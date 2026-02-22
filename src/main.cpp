cf010d031f2a044adb83e456ebe71cbae2758215
rkc137 <hlopov100@gmail.com>
2026-02-22 07:19:25 +0300

    more safe args read

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
     for(int i = 1; i < argc; i++)
     {
         std::string_view arg = argv[i];
-        if(arg[0] != '-')
+        if(!arg.empty() || arg[0] != '-')
         {
             std::cerr << "unknown argument: " << arg << "\n-help to see usage";
             return EXIT_FAILURE;
@@ -39,7 +39,7 @@ int main(int argc, char* argv[])
         else if(i + 1 < argc)
         {
             std::string_view arg_value = argv[++i];
-            if(arg_value[0] == '-')
+            if(arg_value.empty() || arg_value[0] == '-')
                 exit_with_failure("missing argument value: " + std::string(arg));
 
             if(arg == "-sf")
@@ -49,8 +49,12 @@ int main(int argc, char* argv[])
             else if(arg == "-s" || arg == "-sm")
             {
                 size_t value;
-                if(auto result = std::from_chars(arg_value.data(), arg_value.data() + arg_value.size(), value);
-                    result.ec != std::errc())
+                auto result = std::from_chars(
+                    arg_value.data(),
+                    arg_value.data() + arg_value.size(),
+                    value
+                );
+                if(result.ec != std::errc{})
                     exit_with_failure("bad space\n");
                 if(arg == "-sm") value *= 1024 * 1024;
                 target_size = value;
@@ -64,11 +68,14 @@ int main(int argc, char* argv[])
 
     struct {
         std::string name, ext, data;
+        uint64_t size;
     } source_file;
 
-    if(std::ifstream s{source_file_path, std::ios::binary}; s.is_open())
+    if(std::ifstream stream{source_file_path, std::ios::binary}; stream.is_open())
     {
-        source_file.data.assign(std::istreambuf_iterator<char>(s), {});
+        auto s = fs::file_size(source_file_path);
+        source_file.size = s == 0 ? 1 : s;
+        source_file.data.assign(std::istreambuf_iterator<char>{stream}, {});
         source_file.name = source_file_path.stem().string();
         source_file.ext = source_file_path.extension().string();
     }
@@ -80,20 +87,20 @@ int main(int argc, char* argv[])
     }
 
     if(fs::exists(target_folder_path))
+    {
         if(!fs::is_directory(target_folder_path))
             exit_with_failure("target folder is not directory actually");
+    }
     else if(!fs::create_directory(target_folder_path))
         exit_with_failure("failed to create target folder\n");
 
-    auto file_size = fs::file_size(source_file_path);
-    if(file_size == 0) file_size++;
     auto file_count =
         target_size.value_or(fs::space(target_folder_path).available) /
-        file_size;
+        source_file.size;
 
     std::cout
         << "file target count: " << file_count << '\n'
-        << "total spam size: " << (file_count * source_file.data.size()) / (1024 * 1024) << "MBs\n"
+        << "total spam size: " << (file_count * source_file.size) / (1024 * 1024) << "MBs\n"
         << "continue? (N/Y)";
     char YorN;
     std::cin >> YorN;
